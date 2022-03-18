d8010d3b4d0cf2dc9ad41d2fb41488ce6d6edb53
Jaeseok Lee <jaeseok0708@gmail.com>
2022-03-18 22:47:09 +0900

    18 Mar 2022_v2

---
+++ output.py
@@ -0,0 +1,17 @@
+# print one
+print("# Print one thing.")
+print("Hello Python Programming...!")
+print()
+
+# print more than one thing
+print("# Print more than one thing.")
+print(10, 20, 30, 40 ,50)
+print("Hi", "I", "am", "Jaeseok Lee")
+print()
+
+# print nothing.
+print("# Print nothing.")
+print("-----confirmation line-----")
+print()
+print()
+print("-----confirmation line-----")
---
+++ string_operator.py
@@ -0,0 +1,4 @@
+print("name\tage\tregion")
+print("John\t25\tTexas")
+print("Smith\t28\tCalifornia")
+print("Lilly\t3\tNevada")
